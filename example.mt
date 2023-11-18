89c76173ae3cd7678fe33f6c00e187be512dab37
mugambi-victor <victormmugambi@gmail.com>
2023-11-18 11:33:47 +0300

    stacks

--- example.mt
+++ example.mt
@@ -1,4 +1,4 @@
-push 1
-push 2
-push 3
+push 98
+push 897
+queue Holberton
 pall
--- str_tokenizer.c
+++ str_tokenizer.c
@@ -4,15 +4,14 @@
 char *stack_token[MAX_TOKENS];
 
 
-
 /**
- * tokenize - Tokenizes a string based on a delimiter.
- * @input_str: String to be tokenized.
- * @delimiter: Delimiter used for tokenization.
- * @stack_tokens: Array to store the resulting tokens.
- * @max_tokens: Maximum number of tokens to store.
- * @return The number of tokens generated.
- */
+* tokenize - Tokenizes a string based on a delimiter.
+* @input_str: String to be tokenized.
+* @delimiter: Delimiter used for tokenization.
+* @stack_tokens: Array to store the resulting tokens.
+* @max_tokens: Maximum number of tokens to store.
+* @return The number of tokens generated.
+*/
 int tokenize(char *input_str, const char *delimiter, char *stack_tokens[], int max_tokens)
 {
 	char *token = strtok(input_str, delimiter);
@@ -24,5 +23,11 @@ int tokenize(char *input_str, const char *delimiter, char *stack_tokens[], int m
 		stack_tokens[token_count++] = token;
 		token = strtok(NULL, delimiter);
 	}
+
+	if (token_count >= 1 && strcmp(stack_tokens[0], "queue") == 0 && token_count >= 2)
+	{
+		return (0);
+	}
+
 	return (token_count);
 }
